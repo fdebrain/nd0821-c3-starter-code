3fad9f250918b5bd53c9940694a3c82ed849d1cd
fdebrain <12777340+fdebrain@users.noreply.github.com>
2021-11-26 18:58:47 +0100

    linting and missing import

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -2,9 +2,10 @@
 
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+from starter.ml.data import process_data
 
 # Add code to load in the data.
+data = None
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,9 +20,11 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+X_train, y_train, encoder, lb = process_data(train,
+                                             categorical_features=cat_features,
+                                             label="salary",
+                                             training=True
+                                             )
 
 # Proces the test data with the process_data function.
 
